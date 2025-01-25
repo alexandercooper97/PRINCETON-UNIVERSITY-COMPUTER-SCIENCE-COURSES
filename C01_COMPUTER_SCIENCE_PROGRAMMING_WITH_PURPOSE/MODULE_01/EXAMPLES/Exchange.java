0c8798fdf01a1fddd26a07cb218866c84180f0e6
Alexander Cooper <130676645+alexandercooper97@users.noreply.github.com>
2025-01-25 16:41:19 -0500

    MODULE 01

---
+++ C01_COMPUTER_SCIENCE_PROGRAMMING_WITH_PURPOSE/MODULE_01/EXAMPLES/Exchange.java
@@ -0,0 +1,13 @@
+public class Exchange {
+    public static void main(String[] args){
+        int a = 1234;
+        int b = 99;
+        int t = a;
+        a = b;
+        b = t;
+
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
