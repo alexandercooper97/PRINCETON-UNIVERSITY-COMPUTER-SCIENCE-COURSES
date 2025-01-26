5e5813754b6e44b92375c91daeb6b47ae6cbce35
Alexander Cooper <130676645+alexandercooper97@users.noreply.github.com>
2025-01-26 17:32:53 -0500

    MODULE 01

---
+++ C01_COMPUTER_SCIENCE_PROGRAMMING_WITH_PURPOSE/M01_BASIC_PROGRAMMING_CONCETPS/03_ASSIGNMENT/Programming_Assignment/HelloGoodbye.java
@@ -0,0 +1,16 @@
+/* *****************************************************************************
+ *  Name             : Alexander Cooper
+ *  Coursera User ID : c7964f1065c5bb86cdfe4fea01f0cbca
+ *  Date             : January 26, 2025
+ **************************************************************************** */
+
+public class HelloGoodbye {
+
+    public static void main(String[] args) {
+        String name1 = args[0];
+        String name2 = args[1];
+    
+        System.out.println("Hello " + name1 + " and " + name2 + ".");
+        System.out.println("Goodbye " + name2 + " and " + name1 + ".");
+    }
+}
