71631bd66d60b68c50d17d05b45719248b98d1dd
Alexander Cooper <130676645+alexandercooper97@users.noreply.github.com>
2025-01-26 17:33:26 -0500

    MODULE 01

---
+++ C01_COMPUTER_SCIENCE_PROGRAMMING_WITH_PURPOSE/M01_BASIC_PROGRAMMING_CONCETPS/03_ASSIGNMENT/Programming_Assignment/HelloWorld.java
@@ -0,0 +1,10 @@
+/* *****************************************************************************
+ *  Name             : Alexander Cooper
+ *  Coursera User ID : c7964f1065c5bb86cdfe4fea01f0cbca
+ *  Date             : January 26, 2025
+ **************************************************************************** */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
